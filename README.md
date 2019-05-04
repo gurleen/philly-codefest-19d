1417023f9befec7c6be8ffd08a8935b82832870c
Gurleen Singh <gurleen@twitnit.me>
2019-05-04 12:22:07 -0400

    Added basic routes to app.py

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # philly-codefest-19d
+
+To run:
+```
+$ pip install Flask
+$ FLASK_APP=hello.py flask run
+```
--- app.py
+++ app.py
@@ -1,6 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
+def home():
     return 'Hello, world!'
+
+@app.route('/resources/')
+def resources():
+    return 'Resources page'
+
+@app.route('/volunteer/')
+def volunteers():
+    return 'Volunteer page'
+
+@app.route('/submit-resource/', methods=['POST', 'GET'])
+def submit_resource():
+    return 'Submit resource'
+
+@app.route('/submit-volunteer/', methods=['POST', 'GET'])
+def submit_volunteer():
+    return 'Submit volunteer'
+
+@app.route('/about')
+def about():
+    return 'About us'
