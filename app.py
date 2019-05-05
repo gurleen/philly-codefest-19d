85189b1ae721984eabe6482267abdc6c0e8f6176
Gurleen Singh <gurleen@twitnit.me>
2019-05-05 10:59:42 -0400

    Finished

--- app.py
+++ app.py
@@ -15,13 +15,13 @@ def home():
 def resources():
     style = url_for('static', filename='css/flatly.css')
     results = get_resources_matching({})
-    return render_template('list_view.html', results=results, style=style, title='Resources')
+    return render_template('list_view.html', results=results, style=style, title='Resources', is_res=True)
 
 @app.route('/volunteer/')
 def volunteers():
     style = url_for('static', filename='css/flatly.css')
     results = get_Volunteer_Ops_matching({})
-    return render_template('list_view.html', results=results, style=style, title='Volunteer')
+    return render_template('list_view.html', results=results, style=style, title='Volunteer', is_res=False)
 
 @app.route('/submit/', methods=['POST'])
 def submit():
@@ -34,6 +34,28 @@ def submit():
         x = Resource(result['name'], result['category'], result['loc'], result['descrip'], result['email'])
         store_resource(x)
         return redirect(url_for('/resources'))
+
+# /view?id=abc
+@app.route('/view-res/')
+def view_res():
+    style = url_for('static', filename='css/flatly.css')
+    res_id = request.args.get('id')
+    result = get_resources_matching({'_id': res_id})
+    if result == None:
+        return redirect(url_for('home'))
+    else:
+        return render_template('individual.html', style=style, info=result[0].get_json(), is_res=True)
+
+@app.route('/view-vol/')
+def view_vol():
+    style = url_for('static', filename='css/flatly.css')
+    vol_id = request.args.get('id')
+    result = get_Volunteer_Ops_matching({'_id': vol_id})
+    if result == None:
+        return redirect(url_for('home'))
+    else:
+        return render_template('individual.html', style=style, info=result[0].get_json(), is_res=False)
+
 
 @app.route('/about')
 def about():
